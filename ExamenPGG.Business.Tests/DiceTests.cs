f488378e2ca3e474005e587d562ba6722c9aa565
Eliano Verkimpe <Eliano.verkimpe@gmail.com>
2022-08-17 13:54:36 +0200

    Added DiceTests

---
+++ ExamenPGG.Business.Tests/DiceTests.cs
@@ -0,0 +1,35 @@
+﻿using ExamenPGG.Business._00_Interfaces;
+using ExamenPGG.Business._01_Classes;
+
+namespace ExamenPGG.Business.Tests
+{
+    public class DiceTests
+    {
+        IDice dice;
+        [SetUp]
+        public void Setup()
+        {
+            dice = new Dice();
+        }
+
+        [TestCase(1, 1, 6)]
+        [TestCase(1, 1, 6)]
+        [TestCase(1, 1, 6)]
+        [TestCase(2, 2, 12)]
+        [TestCase(2, 2, 12)]
+        [TestCase(2, 2, 12)]
+        [TestCase(3, 3, 18)]
+        [TestCase(3, 3, 18)]
+        [TestCase(3, 3, 18)]
+        public void RollDice_InRangeTest(int rollAmount, int expectedRangeMin, int expectedRangeMax)
+        {
+            // Arrange
+
+            // Act
+            int result = dice.RollDice(rollAmount);
+
+            // Assert
+            Assert.That(result, Is.InRange(expectedRangeMin, expectedRangeMax));
+        }
+    }
+}
