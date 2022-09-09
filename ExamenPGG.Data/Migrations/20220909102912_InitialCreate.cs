8e0c9945b4ea31aafa8938cf62702ae383280987
Eliano Verkimpe <Eliano.verkimpe@gmail.com>
2022-09-09 13:36:43 +0200

    DB migrations added + console app as dummy project

---
+++ ExamenPGG.Data/Migrations/20220909102912_InitialCreate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExamenPGG.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StartTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ThrowsToWin = table.Column<int>(type: "INTEGER", nullable: false),
+                    WinnerId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    IconPath = table.Column<string>(type: "varchar(50)", nullable: false),
+                    GameID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Players_Games_GameID",
+                        column: x => x.GameID,
+                        principalTable: "Games",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_WinnerId",
+                table: "Games",
+                column: "WinnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_GameID",
+                table: "Players",
+                column: "GameID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Players_WinnerId",
+                table: "Games",
+                column: "WinnerId",
+                principalTable: "Players",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Players_WinnerId",
+                table: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
--- ExamenPGG/Program.cs
+++ ExamenPGG/Program.cs
@@ -1,32 +1,42 @@
-﻿using ExamenPGG.Business.Bootup;
-using ExamenPGG.Business.GameObject;
-using Microsoft.Extensions.DependencyInjection;
-using ExamenPGG.Business.Logging;
-using ExamenPGG.Business;
-using ExamenPGG.Business.PlayerObject;
-using ExamenPGG.Business.Squares;
-using ExamenPGG.Business.LeaderBoard;
-using ExamenPGG.Business.Squares.Factory;
+﻿//using ExamenPGG.Business.Bootup;
+//using ExamenPGG.Business.GameObject;
+//using Microsoft.Extensions.DependencyInjection;
+//using ExamenPGG.Business.Logging;
+//using ExamenPGG.Business;
+//using ExamenPGG.Business.PlayerObject;
+//using ExamenPGG.Business.Squares;
+//using ExamenPGG.Business.LeaderBoard;
+//using ExamenPGG.Business.Squares.Factory;
 
-ServiceCollection services = new ServiceCollection();
-RegisterServices(services);
+//ServiceCollection services = new ServiceCollection();
+//RegisterServices(services);
 
-ServiceProvider provider = services.BuildServiceProvider();
-var foo = provider.GetService<Bootstrapper>();
-provider.GetService<IBootstrapper>().Run();
+//ServiceProvider provider = services.BuildServiceProvider();
+//var foo = provider.GetService<Bootstrapper>();
+//provider.GetService<IBootstrapper>().Run();
 
-void RegisterServices(ServiceCollection services)
+//void RegisterServices(ServiceCollection services)
+//{
+//    // ADD SERVICES HERE
+//    services.AddTransient<ILogger, Logger>();
+//    services.AddTransient<IBootstrapper, Bootstrapper>();
+//    services.AddTransient<IBootup, Bootup>();
+//    services.AddTransient<IDice, Dice>();  // TODO INCORRECT!
+//    services.AddTransient<ILeaderBoard, LeaderBoard>();
+//    services.AddTransient<ISquareFactory, SquareFactory>();
+//    services.AddTransient<IPlayerFactory, PlayerFactory>();
+
+//    services.AddSingleton<IGameBoard, GameBoard>();
+//}
+
+namespace ExamenPGG
 {
-    // ADD SERVICES HERE
-    services.AddTransient<ILogger, Logger>();
-    services.AddTransient<IBootstrapper, Bootstrapper>();
-    services.AddTransient<IBootup, Bootup>();
-    services.AddTransient<IDice, Dice>();  // TODO INCORRECT!
-    services.AddTransient<ILeaderBoard, LeaderBoard>();
-    services.AddTransient<ISquareFactory, SquareFactory>();
-    services.AddTransient<IPlayerFactory, PlayerFactory>();
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-    services.AddSingleton<IGameBoard, GameBoard>();
+        }
+    }
 }
-
 
