a577552e0e17280aa10e9e33a8b8be627d987e24
KeithVanW <104157496+KeithVanW@users.noreply.github.com>
2022-09-13 16:40:43 +0200

    Endgame screen

--- ExamenPGG.UI/View/MainPage.xaml.cs
+++ ExamenPGG.UI/View/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ExamenPGG.UI.ViewModel;
+﻿using ExamenPGG.UI.View;
+using ExamenPGG.UI.ViewModel;
 
 namespace ExamenPGG.UI
 {
@@ -9,5 +10,20 @@ namespace ExamenPGG.UI
             InitializeComponent();
             BindingContext = vm;
         }
+
+        private async void Label_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (!(sender as Label).IsVisible)
+            {
+            await GoToEndGameAsync();
+            }
+        }
+        private async Task GoToEndGameAsync()
+        {
+            await Shell.Current.GoToAsync($"{nameof(EndGameView)}");
+        }
+
+
+
     }
 }
